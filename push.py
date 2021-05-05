4523606e2a79227b7c6d7abba1128c387e73e88b
Dhanush b Mohan <65810261+dhanushbmohan7@users.noreply.github.com>
2021-05-05 11:27:42 +0530

    Update push.py

--- push.py
+++ push.py
@@ -1,5 +1,7 @@
 from pushbullet import Pushbullet
 API_KEY='o.gLR4waT3ey199F4g95g8jJUEtkjbAtkl'
+print('program started')
 pb=Pushbullet(API_KEY)
 text='This is a simple push notificatio'
 push=pb.push_note('Important',text)
+print('program ended')
